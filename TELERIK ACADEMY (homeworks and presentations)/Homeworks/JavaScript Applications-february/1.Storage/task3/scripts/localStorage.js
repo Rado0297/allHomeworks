ede9730cec48f935cab1ce85c8d76bda2cc69629
Rado0297 <radoslavradov@gmail.com>
2014-03-13 19:47:54 +0200

    For support of local and session storage on older browsers

---
+++ TELERIK ACADEMY (homeworks and presentations)/Homeworks/JavaScript Applications-february/1.Storage/task3/scripts/localStorage.js
@@ -0,0 +1,39 @@
+﻿function loadPairs() {
+    if (!localStorage.length || localStorage.length == 0) {
+        document.getElementById("pairs-container").innerHTML = "{ key:empty, value=empty}";
+        return;
+    }
+    var resultHTML = "<ul>";
+    for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        resultHTML +=
+        '<li>' +
+        '{<strong>key</strong>: "' + key + '", <br/><strong>value</strong>: "' + localStorage.getItem(key) + '"}' +
+        '</li>';
+    }
+    resultHTML += "</ul>";
+    document.getElementById("pairs-container").innerHTML = resultHTML;
+}
+
+function onSaveButtonClick() {
+    var key = document.getElementById("tb-key").value;
+    var value = document.getElementById("value-text").value;
+    localStorage.setItem(key, value);
+    loadPairs();
+}
+
+function onLoadButtonClick() {
+    var key = document.getElementById("tb-key").value;
+    document.getElementById("value-text").value = localStorage.getItem(key);
+}
+
+function onRemoveButtonClick() {
+    var key = document.getElementById("tb-key").value;
+    localStorage.removeItem(key);
+    loadPairs();
+}
+
+function onClearStorageButtonClick() {
+    localStorage.clear();
+    loadPairs();
+}
---
+++ TELERIK ACADEMY (homeworks and presentations)/Homeworks/JavaScript Applications-february/1.Storage/task3/scripts/sessionStorage.js
@@ -0,0 +1,12 @@
+﻿var loadsContainer;
+window.onload = function onWindowLoaded() {
+    loadsContainer = document.getElementById("loads-container");
+
+    if (!sessionStorage.loadCounter) {
+        sessionStorage.setItem("loadCounter", 0);
+    }
+
+    var loadCounter = sessionStorage.loadCounter;
+    sessionStorage.setItem('loadCounter', parseInt(loadCounter) + 1);
+    loadsContainer.innerHTML = sessionStorage.loadCounter;
+}
