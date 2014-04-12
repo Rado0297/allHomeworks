77d9ad3f750a76b9f62e5ae2cfd57684601088f9
Rado0297 <radoslavradov@gmail.com>
2014-04-12 18:55:49 +0300

    telerik academy task 1 -addition to live demo from letion on 25.03.2014

---
+++ TELERIK ACADEMY (homeworks and presentations)/Homeworks/JavaScript SPA-mart/5.Contrllers and Markup in Angularjs/task1/Music-Artists-master/js/forEditArtists.js
@@ -0,0 +1,47 @@
+﻿function addElement() {
+
+    var ni = document.getElementById('myDiv');
+
+    var numi = document.getElementById('theValue');
+
+    var num = (document.getElementById('theValue').value - 1) + 2;
+
+    numi.value = num;
+
+    var newdiv = document.createElement('div');
+
+    var divIdName = 'Member' + num;
+
+    newdiv.setAttribute('id', divIdName);
+
+    newdiv.innerHTML = '<label for="name" class="col-lg-3 control-label">Member</label>' +
+                    '<div class="col-lg-9">' +
+                        '<input type="text" class="form-control validity" id="Text1" placeholder="Name" required ng-model="artist.name">' +
+                    '</div> <a href=\'#\' onclick=\'removeElement(' + divIdName + ')\'>Remove text area "' + divIdName + '"</a>';
+
+    ni.appendChild(newdiv);
+
+}
+
+function addElement2() {
+    var ni = document.getElementById('Div2');
+
+    var numi = document.getElementById('Hidden1');
+
+    var num = (document.getElementById('Hidden1').value - 1) + 2;
+
+    numi.value = num;
+
+    var newdiv = document.createElement('div');
+
+    var divIdName = 'Albums' + num;
+
+    newdiv.setAttribute('id', divIdName);
+
+    newdiv.innerHTML = '<label for="name" class="col-lg-3 control-label">Member</label>' +
+                    '<div class="col-lg-9">' +
+                        '<input type="text" class="form-control validity" id="Text1" placeholder="Name" required ng-model="artist.name">' +
+                    '</div> <a href=\'#\' onclick=\'removeElement(' + divIdName + ')\'>Remove text area "' + divIdName + '"</a>';
+
+    ni.appendChild(newdiv);
+}
