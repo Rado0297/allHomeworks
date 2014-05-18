8d2c783f2eb8c7f438d26c5ebf12604f923c5511
Rado0297 <radoslavradov@gmail.com>
2014-05-18 17:49:37 +0300

    w

--- TELERIK ACADEMY (homeworks and presentations)/MeanStackApp/MEAN/MeanStackLiveDemo/public/app/phones/phoneDetailsCtrl.js
+++
@@ -1,9 +0,0 @@
-app.controller('PhoneDetailsCtrl', function($scope, $routeParams, cachedPhones){
-    $scope.phone = cachedPhones.query().$promise.then(function(collection){
-        collection.forEach(function(phone){
-            if(phone._id === $routeParams.id){
-                $scope.phone = phone;
-            }
-        });
-    });
-});
