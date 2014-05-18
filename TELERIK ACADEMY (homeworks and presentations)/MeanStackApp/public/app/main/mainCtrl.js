8cde769264d98c83553f482957fed9038f136eef
Rado0297 <radoslavradov@gmail.com>
2014-05-18 17:26:56 +0300

    deleted

--- TELERIK ACADEMY (homeworks and presentations)/MeanStackApp/public/app/main/mainCtrl.js
+++
@@ -1,3 +0,0 @@
-app.controller('MainCtrl', function($scope, cachedPhones){
-    $scope.phones = cachedPhones.query();
-});
--- TELERIK ACADEMY (homeworks and presentations)/MeanStackApp/public/app/phones/phoneResource.js
+++
@@ -1,5 +0,0 @@
-app.factory('PhoneResource', function($resource){
-    var PhoneResource = $resource('/api/phones/:id', {id:'@id'}, { update: {method: 'PUT', isArray: false}});
-
-    return PhoneResource;
-});
