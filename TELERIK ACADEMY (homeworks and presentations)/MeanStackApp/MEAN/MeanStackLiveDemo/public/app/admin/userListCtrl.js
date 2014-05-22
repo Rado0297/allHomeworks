b0706f0614a111a8c84e78da6d15d3d6ffc4b936
Rado0297 <radoslavradov@gmail.com>
2014-05-22 19:43:32 +0300

    Some files are deleted

--- TELERIK ACADEMY (homeworks and presentations)/MeanStackApp/MEAN/MeanStackLiveDemo/public/app/admin/userListCtrl.js
+++
@@ -1,3 +0,0 @@
-app.controller('UserListCtrl', function($scope, UsersResource){
-    $scope.users = UsersResource.query();
-});
--- TELERIK ACADEMY (homeworks and presentations)/MeanStackApp/MEAN/MeanStackLiveDemo/server/controllers/index.js
+++
@@ -1,7 +0,0 @@
-var usersController = require('../controllers/usersController');
-var phonesController = require('../controllers/phonesController');
-
-module.exports = {
-    users: usersController,
-    phones: phonesController
-}
