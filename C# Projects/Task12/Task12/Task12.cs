c1469e79a5d927e91d513f568c62546dc9199d03
Rado0297 <radoslavradov@gmail.com>
2014-11-22 18:25:07 +0200

    2 more tasks are added.

---
+++ C# Projects/Task12/Task12/Task12.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task12
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Write a program that reads the radius r of a circle and prints its perimeter and area.");
+
+			Console.WriteLine ("Enter radius of circle:");
+			double radius = double.Parse(Console.ReadLine ());
+
+			double c = 2 * Math.PI * radius;
+			double area = Math.PI * Math.Pow (radius, 2);
+
+			Console.WriteLine ("Perimeter is " + c + ". Area is " + area);
+		}
+	}
+}
---
+++ C# Projects/Task13/Task13/Task13.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+
+namespace Task13
+{
+	class CompanyName
+	{
+		static void Main()
+		{
+			Console.WriteLine ("A company has name, address, phone number, fax number, web site and manager. The manager has first name, last name, age and a phone number. Write a program that reads the information about a company and its manager and prints them on the console.​");
+
+			Console.OutputEncoding = Encoding.UTF8;
+
+			Console.Write("Enter company name: ");
+			string compName=Console.ReadLine();
+
+			Console.Write("Enter company address: ");
+			string compAddress=Console.ReadLine();
+
+			Console.Write("Enter phone number: ");
+			string compPhoneNum=Console.ReadLine();
+
+			Console.Write("Enter fax number:");
+			string compFaxNum=Console.ReadLine();
+
+			Console.Write("Enter Web site: ");
+			string webSite=Console.ReadLine();
+
+			Console.Write("Enter manager first name: ");
+			string manFirstName=Console.ReadLine();
+
+			Console.Write("Enter manager last name: ");
+			string manLastName=Console.ReadLine();
+
+			Console.Write("Enter manager age: ");
+			int manAge = int.Parse(Console.ReadLine());
+
+			Console.Write("Enter manager phone: ");
+			string ManPhone=Console.ReadLine();
+
+			Console.WriteLine ();
+			Console.WriteLine ("-------------------------------------------------------------------");
+			Console.WriteLine ("Company: {0}", compName);
+			Console.Write(" Address: {0}  {1}", compAddress, Environment.NewLine);
+			Console.WriteLine (" Phone: {0} \n Fax: {1} {2} Web site: {3}", compPhoneNum, compFaxNum, Environment.NewLine, webSite);
+
+			Console.WriteLine ("-------------------------------------------------------------------");
+			Console.Write ("Manager: {0} {1},\n age: {2}{3} Phone: {4} {5}", manFirstName, manLastName, manAge, Environment.NewLine, ManPhone, Environment.NewLine);
+			Console.WriteLine ("-------------------------------------------------------------------");
+		}
+	}
+}
