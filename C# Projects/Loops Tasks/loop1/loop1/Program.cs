6e5451e149520d76643108010c12ff8a4e511c8b
Rado0297 <radoslavradov@gmail.com>
2014-12-12 18:17:25 +0200

    Tasks are separated in folders

---
+++ C# Projects/Loops Tasks/loop1/loop1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace loop1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ C# Projects/Random tasks/Task18/Task18/recArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task18
+{
+	public class recArea
+	{
+		public static double rec(double a, double b)
+		{
+			return a*b;
+		}
+	}
+}
+
