5ee3bfea259f6299d40f7c298d2daa77929f5d93
Rado0297 <radoslavradov@gmail.com>
2014-12-27 21:16:37 +0200

    New class task(C#)

---
+++ C# Projects/classusage/classusage/recArea.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classusage
+{
+	public class recArea
+	{
+		public static double rec(double a, double b)
+		{
+			return a*b;
+		}
+	}
+}
---
+++ C# Projects/classusage/classusage/rectangular.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace classusage
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//For using other classes
+			Console.WriteLine (recArea.rec (6, 6));
+		}
+	}
+}
