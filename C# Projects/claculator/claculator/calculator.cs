824bcc52175c4febf88e8c048e3db614ed7a488d
Rado0297 <radoslavradov@gmail.com>
2015-02-06 18:49:23 +0200

    C# calculator with switch cases.

---
+++ C# Projects/claculator/claculator/calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace claculator
+{
+    class calculator
+    {
+        static void Main(string[] args)
+        {
+            double result;
+
+            Console.WriteLine("Enter first number: ");
+            double firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            double secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter type of operation (+ , - , * , /): ");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+": 
+                    result = firstNumber + secondNumber;
+                    Console.WriteLine("Sum is: " + result);
+                    Console.WriteLine("Wait a few seconds for automatic exit of app.");
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;
+                    Console.WriteLine("Result is: " + result);
+                    Console.WriteLine("Wait a few seconds for automatic exit of app.");
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    Console.WriteLine("Multiple is: " + result);
+                    Console.WriteLine("Wait a few seconds for automatic exit of app.");
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    Console.WriteLine("Devision result is: " + result);
+                    Console.WriteLine("Wait a few seconds for automatic exit of app.");
+                    break;
+                default: 
+                    Console.WriteLine("Wrong operation!");
+                    Console.WriteLine("Wait a few seconds for automatic exit of app.");
+                    break;
+            }
+
+            System.Threading.Thread.Sleep(50000);
+        }
+    }
+}
