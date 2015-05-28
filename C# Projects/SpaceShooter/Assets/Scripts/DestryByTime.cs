b843ed4860293a02d2294ba142f41e64e898a614
Rado0297 <radoslavradov@gmail.com>
2015-05-28 22:33:06 +0300

    Game almost ready

---
+++ C# Projects/SpaceShooter/Assets/Scripts/DestryByTime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestryByTime : MonoBehaviour 
+{
+	public float lifeTime;
+
+	void Start()
+	{
+		Destroy (gameObject, lifeTime);
+	}
+}
---
+++ C# Projects/SpaceShooter/Assets/Scripts/RadndomRotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadndomRotator : MonoBehaviour 
+{
+	public float tumble;
+
+	void Start()
+	{
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
