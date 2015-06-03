5a7797efe9e4719f1666a03a6c46164d46db42df
Rado0297 <radoslavradov@gmail.com>
2015-06-03 20:02:30 +0300

    Added some new levels

---
+++ C# Projects/Breakout game/Assets/Main Menu/Features.js
@@ -0,0 +1,6 @@
+﻿//Features of the game
+/*
+1. Add slecet level to main Menu
+2. Restart of game to the beginnig instead of to current level :D
+3. Add some kind of score
+*/
--- C# Projects/Breakout game/Assets/Scripts/GM.cs
+++ C# Projects/Breakout game/Assets/Scripts/GM.cs
@@ -8,9 +8,11 @@ public class GM : MonoBehaviour {
 	public int lives = 3;
 	public int bricks = 30;
 	public float resetDelay = 1f;
+	public int numberOfLevels = 7; //change this value when make more levels !!!
 	public Text livesText;
 	public GameObject GameOver;
 	public GameObject YouWon;
+	public GameObject waitTxt;
 	public GameObject bricksPrefab;
 	public GameObject paddle;
 	public GameObject deathParticles;
@@ -44,9 +46,23 @@ public class GM : MonoBehaviour {
 	{
 		if (bricks < 1)
 		{
-			YouWon.SetActive(true);
-			Time.timeScale = .25f;
-			Invoke("NextLevel", 1f);
+			//Needs debugging for last level
+			if(Application.loadedLevel == numberOfLevels)
+			{
+				YouWon.SetActive(true);
+				waitTxt.SetActive(false);
+			}
+			else
+			{
+				YouWon.SetActive(false);
+				waitTxt.SetActive(true);
+				Time.timeScale = .25f;
+				Invoke("NextLevel", 1f);
+			}
+			//YouWon.SetActive(true);
+			//waitTxt.SetActive(true);
+			//Time.timeScale = .25f;
+			//Invoke("NextLevel", 1f);
 
 			//Invoke("Reset", resetDelay);
 			//This is for going to next level, but it needs to be setted in corect order
