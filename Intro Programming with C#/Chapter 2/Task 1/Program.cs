9b6efd38cbd0bf0418e9bd385c7c32635a2dd61a
Rado0297 <radoslavradov@gmail.com>
2015-06-18 23:47:49 +0300

    new task added

---
+++ Intro Programming with C#/Chapter 2/Task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            sbyte num1 = -115;
+            sbyte num5 = -44;
+            byte num2 = 112;
+            byte num3 = 97;
+            byte num4 = 224;
+            short num6 = -10000;
+            short num7 = 20000;
+            short num8 = 1990;
+            ushort num9 = 52130;
+            int num10 = 4825932;
+            int num11 = -1000000;
+            uint num12 = 970700000;
+            long num13 = 123456789123456789;
+            ulong num14 = 123456789123456789;
+        }
+    }
+}
