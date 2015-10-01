c7e33aed2591bc9de6710352a3feb875a18a564e
Rado0297 <radoslavradov@gmail.com>
2015-10-01 19:38:29 +0300

    Form application for school presentation for tomorrow.

---
+++ C# Windows Forms projects/FormAppChangeProperties/FormAppChangeProperties/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormAppChangeProperties
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "My Form Application Title";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.Color.YellowGreen;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Text = "";
+            button2.Text = "";
+        }
+    }
+}
